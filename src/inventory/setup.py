808e84fa96a768fa4a359534598791ff535c06cd
goschtl <ck@novareto.de>
2014-02-20 09:38:58 +0100

    Initial Import

---
+++ src/inventory/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='inventory',
+      version=version,
+      description="",
+      long_description=""" """,
+      classifiers=[],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points={
+         'fanstatic.libraries': [
+            'inventory = inventory.resources:library',
+         ],
+         'paste.app_factory': [
+             'app = inventory.utils:Application',
+         ],
+      }
+      )
